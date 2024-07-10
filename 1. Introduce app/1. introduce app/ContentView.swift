6e2773c02783773f303e20c6f598a6a9d3c84028
jeseon-archive <jooj8087@gmail.com>
2024-07-11 08:49:50 +0900

    dog app 1

---
+++ 1. Introduce app/1. introduce app/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  1. introduce app
+//
+//  Created by lee on 7/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack{
+                Image("dogProfile")
+                    .resizable()
+                    .frame(width: 120, height: 120)
+                    .clipShape(Circle())
+                    .overlay(Circle()
+                        .stroke(Color.teal, lineWidth: 2)
+                    )
+                
+                VStack(alignment: .leading)
+                {
+                    Text("이름: 몽이")
+                    Text("푸들")
+                    Text("2023년생")
+                }
+                .font(.system(size: 20))
+                .padding(.leading, 20)
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 1. Introduce app/1. introduce app/___introduce_appApp.swift
@@ -0,0 +1,12 @@
+//___FILEHEADER___
+
+import SwiftUI
+
+@main
+struct introduce_app: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
